e427e952fd35f57523dcb2670bce83e8ad9c106a
Archy Agent <archy@vhl.ai>
2026-07-05 21:16:51 +0530

    Completed circuit synthesis for communication-bridge

--- communication-bridge/communication-bridge.tsx
+++ communication-bridge/communication-bridge.tsx
@@ -4,146 +4,121 @@ import { ACT45B_101_2P_TL003 } from "./imports/ACT45B_101_2P_TL003"
 
 export const CommunicationBridge = () => {
   return (
-    <board>
-      {/* Bridge IC: BQ79600-Q1 */}
-      <BQ79600PWRQ1 name="U1" />
+    <board width="100mm" height="80mm">
+      <pinheader
+        name="P1"
+        pinCount={5}
+        pcbX={50}
+        pcbY={0}
+        schX={10}
+        schY={0}
+        pinLabels={["1_P", "1_N", "2_P", "2_N", "3_CVDD"]}
+      />
+
+      <pinheader
+        name="P2"
+        pinCount={11}
+        pcbX={-50}
+        pcbY={0}
+        schX={-10}
+        schY={0}
+        pinLabels={["1_BAT", "2_VIO", "3_GND", "4_SCLK", "5_nCS", "6_MOSI", "7_MISO", "8_nUART_SPI", "9_NFAULT", "10_INH", "11_DVDD"]}
+      />
+
+      {/* Component declarations */}
+      <BQ79600PWRQ1 name="U1" pcbX={0} pcbY={0} schX={0} schY={0} />
 
       {/* Isolation Transformers for Daisy Chain */}
-      <HX1188NLT name="T1" />
-      <HX1188NLT name="T2" />
+      <HX1188NLT name="T1" pcbX={25} pcbY={15} schX={0} schY={5} />
+      <HX1188NLT name="T2" pcbX={25} pcbY={-15} schX={0} schY={-5} />
 
       {/* Common Mode Chokes for EMC */}
-      <ACT45B_101_2P_TL003 name="L1" />
-      <ACT45B_101_2P_TL003 name="L2" />
+      <ACT45B_101_2P_TL003 name="L1" pcbX={45} pcbY={15} schX={5} schY={5} />
+      <ACT45B_101_2P_TL003 name="L2" pcbX={45} pcbY={-15} schX={5} schY={-5} />
 
       {/* Power Supply Decoupling Capacitors */}
-      <capacitor
-        name="C1"
-        capacity="0.1uF"
-        footprint="0603"
-        voltageRating="50V"
-      />
-      <capacitor
-        name="C2"
-        capacity="10uF"
-        footprint="0805"
-        voltageRating="50V"
-      />
-      <capacitor
-        name="C10"
-        capacity="4.7uF"
-        footprint="0603"
-        voltageRating="10V"
-      />
-      <capacitor
-        name="C11"
-        capacity="0.1uF"
-        footprint="0402"
-        voltageRating="10V"
-      />
-      <capacitor
-        name="C12"
-        capacity="1uF"
-        footprint="0402"
-        voltageRating="10V"
-      />
+      <capacitor name="C1" capacitance="0.1uF" footprint="0603" pcbX={-10} pcbY={10} schX={-15} schY={5} />
+      <capacitor name="C2" capacitance="10uF" footprint="0805" pcbX={-10} pcbY={15} schX={-15} schY={8} />
+      <capacitor name="C10" capacitance="4.7uF" footprint="0603" pcbX={-10} pcbY={-10} schX={-10} schY={-10} />
+      <capacitor name="C11" capacitance="0.1uF" footprint="0402" pcbX={-10} pcbY={-15} schX={-10} schY={-13} />
+      <capacitor name="C12" capacitance="1uF" footprint="0402" pcbX={-10} pcbY={-20} schX={-10} schY={-16} />
 
       {/* Configuration and Host Interface Pull-up/down Resistors */}
-      <resistor name="R1" resistance="10k" footprint="0402" />{" "}
-      {/* nCS pull-up to VIO */}
-      <resistor name="R2" resistance="10k" footprint="0402" />{" "}
-      {/* RX/MOSI pull-up to VIO */}
-      <resistor name="R3" resistance="10k" footprint="0402" />{" "}
-      {/* SCLK pull-down to GND */}
+      <resistor name="R1" resistance="10k" footprint="0402" pcbX={-20} pcbY={10} schX={-15} schY={-2} />
+      <resistor name="R2" resistance="10k" footprint="0402" pcbX={-20} pcbY={15} schX={-15} schY={-5} />
+      <resistor name="R3" resistance="10k" footprint="0402" pcbX={-20} pcbY={20} schX={-15} schY={-8} />
       {/* Daisy Chain Differential Termination (Pin Side) */}
-      <resistor name="R4" resistance="49.9" footprint="0402" />
-      <resistor name="R5" resistance="49.9" footprint="0402" />
-      <resistor name="R6" resistance="49.9" footprint="0402" />
-      <resistor name="R7" resistance="49.9" footprint="0402" />
-
-      {/* Main Power and Ground Nets */}
-      <net name="GND" />
-      <net name="BAT" />
-      <net name="VIO" />
-      <net name="CVDD" />
-      <net name="DVDD" />
+      <resistor name="R4" resistance="49.9" footprint="0402" pcbX={15} pcbY={10} schX={-2} schY={8} />
+      <resistor name="R5" resistance="49.9" footprint="0402" pcbX={15} pcbY={5} schX={2} schY={8} />
+      <resistor name="R6" resistance="49.9" footprint="0402" pcbX={15} pcbY={-5} schX={-2} schY={-8} />
+      <resistor name="R7" resistance="49.9" footprint="0402" pcbX={15} pcbY={-10} schX={2} schY={-8} />
 
       {/* Power Connectivity */}
-      <trace from=".U1 > BAT" to="net.BAT" />
-      <trace from=".U1 > VIO" to="net.VIO" />
-      <trace from=".U1 > GND" to="net.GND" />
-      <trace from=".U1 > CVDD" to="net.CVDD" />
-      <trace from=".U1 > DVDD" to="net.DVDD" />
+      <trace name="N_BAT" from="U1.pin15" to="C1.pin1" />
+      <trace name="N_BAT" from="C1.pin1" to="C2.pin1" />
+      <trace name="N_BAT" from="C2.pin1" to="P2.pin1" />
 
-      {/* Decoupling Connectivity */}
-      <trace from=".C1 > pin1" to="net.BAT" />
-      <trace from=".C1 > pin2" to="net.GND" />
-      <trace from=".C2 > pin1" to="net.BAT" />
-      <trace from=".C2 > pin2" to="net.GND" />
-      <trace from=".C10 > pin1" to="net.CVDD" />
-      <trace from=".C10 > pin2" to="net.GND" />
-      <trace from=".C11 > pin1" to="net.VIO" />
-      <trace from=".C11 > pin2" to="net.GND" />
-      <trace from=".C12 > pin1" to="net.DVDD" />
-      <trace from=".C12 > pin2" to="net.GND" />
+      <trace name="N_VIO" from="U1.pin3" to="C11.pin1" />
+      <trace name="N_VIO" from="C11.pin1" to="R1.pin2" />
+      <trace name="N_VIO" from="R1.pin2" to="R2.pin2" />
+      <trace name="N_VIO" from="R2.pin2" to="P2.pin2" />
+
+      <trace name="N_GND" from="U1.pin9" to="C1.pin2" />
+      <trace name="N_GND" from="C1.pin2" to="C2.pin2" />
+      <trace name="N_GND" from="C2.pin2" to="C10.pin2" />
+      <trace name="N_GND" from="C10.pin2" to="C11.pin2" />
+      <trace name="N_GND" from="C11.pin2" to="C12.pin2" />
+      <trace name="N_GND" from="C12.pin2" to="R3.pin2" />
+      <trace name="N_GND" from="R3.pin2" to="P2.pin3" />
+
+      <trace name="N_CVDD" from="U1.pin14" to="C10.pin1" />
+      <trace name="N_CVDD" from="C10.pin1" to="R4.pin2" />
+      <trace name="N_CVDD" from="R4.pin2" to="R6.pin2" />
+      <trace name="N_CVDD" from="R6.pin2" to="T1.pin15" />
+      <trace name="N_CVDD" from="T1.pin15" to="T2.pin15" />
+      <trace name="N_CVDD" from="T2.pin15" to="P1.pin5" />
+
+      <trace name="N_DVDD" from="U1.pin1" to="C12.pin1" />
+      <trace name="N_DVDD" from="C12.pin1" to="P2.pin11" />
 
       {/* Host Interface Connectivity (SPI Mode Configuration) */}
-      <trace from=".U1 > SCLK" to=".R3 > pin1" />
-      <trace from=".R3 > pin2" to="net.GND" />
+      <trace name="SCLK" from="U1.pin6" to="R3.pin1" />
+      <trace name="SCLK" from="U1.pin6" to="P2.pin4" />
 
-      <trace from=".U1 > nCS" to=".R1 > pin1" />
-      <trace from=".R1 > pin2" to="net.VIO" />
+      <trace name="nCS" from="U1.pin7" to="R1.pin1" />
+      <trace name="nCS" from="U1.pin7" to="P2.pin5" />
 
-      <trace from=".U1 > pin4" to=".R2 > pin1" /> {/* RX/MOSI */}
-      <trace from=".R2 > pin2" to="net.VIO" />
+      <trace name="MOSI" from="U1.pin4" to="R2.pin1" />
+      <trace name="MOSI" from="U1.pin4" to="P2.pin6" />
+      
+      <trace name="MISO" from="U1.pin5" to="P2.pin7" />
 
       {/* COMH Daisy Chain Signal Path */}
-      {/* Termination resistors with center-tap to CVDD */}
-      <trace from=".U1 > COMHP" to=".R4 > pin1" />
-      <trace from=".R4 > pin2" to=".R5 > pin1" />
-      <trace from=".R5 > pin2" to=".U1 > COMHN" />
-      <trace from=".R4 > pin2" to="net.CVDD" />
+      <trace name="COMH_P" from="U1.pin13" to="R4.pin1" />
+      <trace name="COMH_P" from="U1.pin13" to="T1.pin16" />
+      <trace name="COMH_P" from="T1.pin1" to="L1.pin1" />
+      <trace name="COMH_P" from="L1.pin1" to="P1.pin1" />
 
-      {/* Transformer coupling for COMH */}
-      <trace from=".U1 > COMHP" to=".T1 > pin16" />
-      <trace from=".U1 > COMHN" to=".T1 > pin14" />
-      <trace from=".T1 > pin15" to="net.CVDD" />
-
-      <trace from=".T1 > pin1" to=".L1 > pin1" />
-      <trace from=".T1 > pin3" to=".L1 > pin2" />
+      <trace name="COMH_N" from="U1.pin12" to="R5.pin1" />
+      <trace name="COMH_N" from="R5.pin2" to="T1.pin14" />
+      <trace name="COMH_N" from="T1.pin3" to="L1.pin2" />
+      <trace name="COMH_N" from="L1.pin2" to="P1.pin2" />
 
       {/* COML Daisy Chain Signal Path */}
-      {/* Termination resistors with center-tap to CVDD */}
-      <trace from=".U1 > COMLP" to=".R6 > pin1" />
-      <trace from=".R6 > pin2" to=".R7 > pin1" />
-      <trace from=".R7 > pin2" to=".U1 > COMLN" />
-      <trace from=".R6 > pin2" to="net.CVDD" />
+      <trace name="COML_P" from="U1.pin10" to="R6.pin1" />
+      <trace name="COML_P" from="U1.pin10" to="T2.pin16" />
+      <trace name="COML_P" from="T2.pin1" to="L2.pin1" />
+      <trace name="COML_P" from="L2.pin1" to="P1.pin3" />
 
-      {/* Transformer coupling for COML */}
-      <trace from=".U1 > COMLP" to=".T2 > pin16" />
-      <trace from=".U1 > COMLN" to=".T2 > pin14" />
-      <trace from=".T2 > pin15" to="net.CVDD" />
+      <trace name="COML_N" from="U1.pin11" to="R7.pin1" />
+      <trace name="COML_N" from="R7.pin2" to="T2.pin14" />
+      <trace name="COML_N" from="T2.pin3" to="L2.pin2" />
+      <trace name="COML_N" from="L2.pin2" to="P1.pin4" />
 
-      <trace from=".T2 > pin1" to=".L2 > pin1" />
-      <trace from=".T2 > pin3" to=".L2 > pin2" />
-
-      {/* System Level Port Mappings (from Module Boundary) */}
-      {/* Daisy Chain Interface (Hot Side) */}
-      <net name="P1_1_P" /> <trace from=".L1 > pin4" to="net.P1_1_P" />
-      <net name="P1_1_N" /> <trace from=".L1 > pin3" to="net.P1_1_N" />
-      <net name="P1_2_P" /> <trace from=".L2 > pin4" to="net.P1_2_P" />
-      <net name="P1_2_N" /> <trace from=".L2 > pin3" to="net.P1_2_N" />
-      {/* P1.3 is net.CVDD */}
-
-      {/* Host & Power Interface (Cold Side) */}
-      <net name="P2_4_SCLK" /> <trace from=".U1 > SCLK" to="net.P2_4_SCLK" />
-      <net name="P2_5_nCS" /> <trace from=".U1 > nCS" to="net.P2_5_nCS" />
-      <net name="P2_6_MOSI" /> <trace from=".U1 > pin4" to="net.P2_6_MOSI" />
-      <net name="P2_7_MISO" /> <trace from=".U1 > pin5" to="net.P2_7_MISO" />
-      <net name="P2_8_nUART_SPI" /> <trace from=".U1 > pin8" to="net.P2_8_nUART_SPI" />
-      <net name="P2_9_NFAULT" /> <trace from=".U1 > NFAULT" to="net.P2_9_NFAULT" />
-      <net name="P2_10_INH" /> <trace from=".U1 > INH" to="net.P2_10_INH" />
-      {/* P2.1, P2.2, P2.3, P2.11 are net.BAT, net.VIO, net.GND, net.DVDD respectively */}
+      {/* System Level Port Mappings (Additional) */}
+      <trace name="nUART_SPI" from="U1.pin8" to="P2.pin8" />
+      <trace name="NFAULT" from="U1.pin2" to="P2.pin9" />
+      <trace name="INH" from="U1.pin16" to="P2.pin10" />
     </board>
   )
 }
